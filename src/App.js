ac0bfa2b04071bbf9d5715e54411e0987f02eec7
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2022-12-11 22:44:07 -0300

    ✨ Add React.lazy

--- src/App.js
+++ src/App.js
@@ -1,28 +1,99 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import BookList from "./pages/BookList";
-import Home from "./pages/Home";
-import BookDetails from "./pages/BookDetails";
 import Layout from "./Layout";
-import Form from "./components/Form";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
+// import Home from "./pages/Home";
+// import LoginPage from "./pages/LoginPage";
+// import RegisterPage from "./pages/RegisterPage";
+// import BookList from "./pages/BookList";
+// import BookDetails from "./pages/BookDetails";
+// import Form from "./components/Form";
+// import NotFound404 from "./pages/NotFound404";
+
+// const Layout = lazy(() => import("./Layout"));
+const Home = lazy(() => import("./pages/Home"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const BookList = lazy(() => import("./pages/BookList"));
+const BookDetails = lazy(() => import("./pages/BookDetails"));
+const Form = lazy(() => import("./components/Form"));
+const NotFound404 = lazy(() => import("./pages/NotFound404"));
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="books" element={<BookList />}>
-          <Route path="add-book" element={<Form />} />
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Layout />
+          </Suspense>
+        }
+      >
+        <Route
+          index
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="login"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <LoginPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="register"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <RegisterPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="books"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <BookList />
+            </Suspense>
+          }
+        >
+          <Route
+            path="add-book"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Form />
+              </Suspense>
+            }
+          />
         </Route>
-        <Route path="books/:bookId" element={<BookDetails />}>
-          <Route path="edit-book" element={<Form />} />
+        <Route
+          path="books/:bookId"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <BookDetails />
+            </Suspense>
+          }
+        >
+          <Route
+            path="edit-book"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Form />
+              </Suspense>
+            }
+          />
         </Route>
         <Route
           path="*"
-          element={<div className="not-found">Not Found 404</div>}
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <NotFound404 />
+            </Suspense>
+          }
         />
       </Route>
     </Routes>
---
+++ src/pages/NotFound404.js
@@ -0,0 +1,5 @@
+function NotFound404() {
+  return <div className="not-found">Not Found 404</div>;
+}
+
+export default NotFound404;
